0d79330d4a95d1a27892b17ef5dc4ce1b7fc1371
Vikas Dagar <vikasdagar@hotmail.com>
2020-04-03 15:27:02 +0100

    Button Code to Link

---
+++ violet/Login_v13/js/button.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    document.getElementById("SignUp").onclick = function () {
+        location.href = "file:///C:/Mihika/Github/covidchallenge/CovidChallenge/violet/index.html"
+    };
+</script>
